82cf41c4aa9ab6712a210fc86abee96e436a326a
zuoguangxing <wangji@v-ver.com>
2014-08-31 02:28:17 +0800

    0.004

---
+++ V-verPlatform/Content/js/Loadingjs.js
@@ -0,0 +1,9 @@
+﻿function initPage()
+{   
+    var objLoading = document.getElementById("LoadingBar");   
+    if (objLoading != null)   
+    {   
+        objLoading.style.display = "none";   
+    }   
+}  
+window.onload = initPage();
