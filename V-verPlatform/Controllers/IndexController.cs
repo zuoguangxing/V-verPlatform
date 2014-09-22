7482ccc786bcf39dd1a7cb23dac95dc92ddaa733
zuoguangxing <wangji@v-ver.com>
2014-09-22 21:24:49 +0800

    0.010

--- V-verPlatform/Controllers/IndexController.cs
+++ V-verPlatform/Controllers/IndexController.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using V_verPlatform.Models.DB;
 namespace V_verPlatform.Controllers
 {
     public class IndexController : Controller
     {
         //
         // GET: /Index/
-
+        userDBEntities userdb = new userDBEntities();
         public ActionResult Index()
         {
             return View();
         }
-
     }
 }
---
+++ V-verPlatform/Controllers/ProgromController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace V_verPlatform.Controllers
+{
+    public class ProgramController : Controller
+    {
+        //
+        // GET: /Progrom/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
