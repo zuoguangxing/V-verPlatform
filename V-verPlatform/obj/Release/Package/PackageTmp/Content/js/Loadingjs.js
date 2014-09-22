7538d837a8695db9d08ee9bcc72b6830bad4618f
zuoguangxing <wangji@v-ver.com>
2014-09-22 10:26:05 +0800

    Revert "0.009"
    This reverts commit 01216f50e0405747251e463e85dd07584909d7dd.

--- V-verPlatform/obj/Release/Package/PackageTmp/Content/js/Loadingjs.js
+++
@@ -1,9 +0,0 @@
-﻿function initPage()
-{   
-    var objLoading = document.getElementById("LoadingBar");   
-    if (objLoading != null)   
-    {   
-        objLoading.style.display = "none";   
-    }   
-}  
-window.onload = initPage();
